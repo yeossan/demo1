55b478e677565e495a29e24481eb69f86e4dd89c
yeossan <mryosan0416@gmail.com>
2024-06-11 11:46:46 +0900

    push aws

---
+++ src/main/java/com/example/demo/controller/TodoController.java
@@ -0,0 +1,43 @@
+package com.example.demo.controller;
+
+import com.example.demo.entity.Todo;
+import com.example.demo.service.TodoService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/todos")
+@RequiredArgsConstructor
+public class TodoController {
+    private final TodoService todoService;
+
+    @GetMapping
+    public List<Todo> getTodos(){
+        return todoService.getTodos();
+    }
+    @GetMapping("/{id}")
+    public Todo getTodo(@PathVariable("id") Long id){
+        return todoService.getTodo(id);
+    }
+    @PostMapping
+    public Todo addTodo(@RequestBody Todo todo){
+        return todoService.addTodo(todo.getTodo());
+    }
+    //put vs patch    --  수정    put - 자체가 바뀔때..  patch  --  부분만 수정될때
+    @PatchMapping("/{id}")
+    public Todo updateTodoById(@PathVariable("id") Long id){
+        return todoService.updateTodo(id);
+    }
+
+    @PatchMapping
+    public Todo updateTodo(@RequestBody Todo todo){
+        return todoService.updateTodo(todo.getId());
+    }
+    @DeleteMapping
+    public String deleteTodo(@RequestBody Todo todo){
+        todoService.deleteTodo(todo.getId());
+        return "ok";
+    }
+}
